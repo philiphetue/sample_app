f3a030fad304418d320418eca8db531c793e2be4
Philip Hetue <philiphetue@gmail.com>
2013-07-18 10:23:53 -0500

    Add ability to follow/unfollow users, add views for followers/followed users, implement Ajax follow/unfollow buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 SampleApp::Application.routes.draw do
-  resources :users
-  resources :sessions,   only: [:new, :create, :destroy]
-  resources :microposts, only: [:create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :sessions,      only: [:new, :create, :destroy]
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
@@ -50,7 +55,7 @@ SampleApp::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
